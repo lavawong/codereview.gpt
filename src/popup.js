3c2b3da8e90472b2fe26f29ffafe6abda2232721
Lava Wong <lazy.false@gmail.com>
2023-04-20 10:11:19 +0800

    feat: Add CodeSmell test

--- src/popup.js
+++ src/popup.js
@@ -11,32 +11,32 @@ const spinner = `
           <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
           <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
         </svg>
-`
+`;
 const checkmark = `
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-green-600">
           <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
-`
+`;
 const xcircle = `
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-red-600">
           <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
-`
+`;
 
 function inProgress(ongoing, failed = false, rerun = true) {
   if (ongoing) {
-    document.getElementById('status-icon').innerHTML = spinner
-    document.getElementById('rerun-btn').classList.add("invisible");
-    document.getElementById('codeball-link').classList.add("invisible");
+    document.getElementById('status-icon').innerHTML = spinner;
+    document.getElementById('rerun-btn').classList.add('invisible');
+    document.getElementById('codeball-link').classList.add('invisible');
   } else {
     if (failed) {
-      document.getElementById('status-icon').innerHTML = xcircle
+      document.getElementById('status-icon').innerHTML = xcircle;
     } else {
-      document.getElementById('status-icon').innerHTML = checkmark
+      document.getElementById('status-icon').innerHTML = checkmark;
     }
     if (rerun) {
-      document.getElementById('rerun-btn').classList.remove("invisible");
-      document.getElementById('codeball-link').classList.remove("invisible");
+      document.getElementById('rerun-btn').classList.remove('invisible');
+      document.getElementById('codeball-link').classList.remove('invisible');
     }
   }
 }
@@ -47,7 +47,7 @@ async function getApiKey() {
   });
   console.log(options);
   if (!options || !options['openai_apikey']) {
-    throw new Error("UNAUTHORIZED");
+    throw new Error('UNAUTHORIZED');
   }
   return options['openai_apikey'];
 }
@@ -57,17 +57,19 @@ async function callChatGPT(messages, callback, onDone) {
   try {
     apiKey = await getApiKey();
   } catch (e) {
-    callback('Please add your Open AI API key to the settings of this Chrome Extension.');
+    callback(
+      'Please add your Open AI API key to the settings of this Chrome Extension.'
+    );
     onDone();
     return;
   }
 
   const api = new ChatGPTAPI({
     apiKey: apiKey,
-    systemMessage: `You are a programming code change reviewer, provide feedback on the code changes given. Do not introduce yourselves.`
-  })
+    systemMessage: `You are a programming code change reviewer, provide feedback on the code changes given. Do not introduce yourselves.`,
+  });
 
-  let res
+  let res;
   let iterations = messages.length;
   for (const message of messages) {
     iterations--;
@@ -78,41 +80,46 @@ async function callChatGPT(messages, callback, onDone) {
       if (iterations == 0) {
         options = {
           onProgress: (partialResponse) => callback(partialResponse.text),
-        }
+        };
       }
       // In progress
       else {
         options = {
-          onProgress: () => callback("Processing your code changes. Number of prompts left to send: " + iterations + ". Stay tuned..."),
-        }
+          onProgress: () =>
+            callback(
+              'Processing your code changes. Number of prompts left to send: ' +
+                iterations +
+                '. Stay tuned...'
+            ),
+        };
       }
 
       if (res) {
-        options.parentMessageId = res.id
+        options.parentMessageId = res.id;
       }
-      res = await api.sendMessage(message, options)
-    } catch (e){
+      res = await api.sendMessage(message, options);
+    } catch (e) {
       callback(String(e));
       onDone();
       return;
     }
-  };
+  }
 
   onDone();
 }
 
 const showdown = require('showdown');
-const converter = new showdown.Converter()
+const converter = new showdown.Converter();
 
 async function reviewPR(diffPath, context, title) {
-  inProgress(true)
-  document.getElementById('result').innerHTML = ''
-  chrome.storage.session.remove([diffPath])
-
+  console.log('reviewPR', context);
+  inProgress(true);
+  document.getElementById('result').innerHTML = '';
+  chrome.storage.session.remove([diffPath]);
 
   let promptArray = [];
   // Fetch the patch from our provider.
-  let patch = await fetch (diffPath).then((r) => r.text())
+  let patch = await fetch(diffPath).then((r) => r.text());
   let warning = '';
   let patchParts = [];
 
@@ -120,6 +127,7 @@ async function reviewPR(diffPath, context, title) {
 
     Your task is:
     - Review the code changes and provide feedback.
+    - Check out the duplicated code in files.
     - If there are any bugs, highlight them.
     - Provide details on missed use of best-practices.
     - Does the code do what it says in the commit messages?
@@ -128,8 +136,7 @@ async function reviewPR(diffPath, context, title) {
     - Provide security recommendations if there are any.
 
     You are provided with the code changes (diffs) in a unidiff format.
-    Do not provide feedback yet. I will follow-up with a description of the change in a new message.`
-  );
+    Do not provide feedback yet. I will follow-up with a description of the change in a new message.`);
 
   promptArray.push(`A description was given to help you assist in understand why these changes were made.
     The description was provided in a markdown format. Do not provide feedback yet. I will follow-up with the code changes in diff format in a new message.
@@ -138,8 +145,8 @@ async function reviewPR(diffPath, context, title) {
 
   // Remove binary files as those are not useful for ChatGPT to provide a review for.
   // TODO: Implement parse-diff library so that we can remove large lock files or binaries natively.
-  const regex = /GIT\sbinary\spatch(.*)literal\s0/mgis;
-  patch = patch.replace(regex,'')
+  const regex = /GIT\sbinary\spatch(.*)literal\s0/gims;
+  patch = patch.replace(regex, '');
 
   // Separate the patch in different pieces to give ChatGPT more context.
   // Additionally, truncate the part of the patch if it is too big for ChatGPT to handle.
@@ -149,88 +156,101 @@ async function reviewPR(diffPath, context, title) {
   // This means we have 16384, and let's reduce 1000 chars from that.
   var files = parsediff(patch);
 
-    // Rebuild our patch as if it were different patches
-    files.forEach(function(file) {
-
-      // Ignore lockfiles
-    if (file.from.includes("lock.json")) {
+  // Rebuild our patch as if it were different patches
+  files.forEach(function (file) {
+    // Ignore lockfiles
+    if (file.from.includes('lock.json')) {
       return;
     }
 
     var patchPartArray = [];
 
-    patchPartArray.push("```diff");
-    if ("from" in file && "to" in file) {
-      patchPartArray.push("diff --git a" + file.from + " b"+ file.to);
+    patchPartArray.push('```diff');
+    if ('from' in file && 'to' in file) {
+      patchPartArray.push('diff --git a' + file.from + ' b' + file.to);
     }
-    if ("new" in file && file.new === true && "newMode" in file) {
-      patchPartArray.push("new file mode " + file.newMode);
+    if ('new' in file && file.new === true && 'newMode' in file) {
+      patchPartArray.push('new file mode ' + file.newMode);
     }
-    if ("from" in file) {
-      patchPartArray.push("--- " + file.from);
+    if ('from' in file) {
+      patchPartArray.push('--- ' + file.from);
     }
-    if ("to" in file) {
-      patchPartArray.push("+++ " + file.to);
+    if ('to' in file) {
+      patchPartArray.push('+++ ' + file.to);
     }
-    if ("chunks" in file) {
-      patchPartArray.push(file.chunks.map(c => c.changes.map(t => t.content).join("\n")));
+    if ('chunks' in file) {
+      patchPartArray.push(
+        file.chunks.map((c) => c.changes.map((t) => t.content).join('\n'))
+      );
     }
-    patchPartArray.push("```");
-    patchPartArray.push("\nDo not provide feedback yet. I will confirm once all code changes were submitted.");
+    patchPartArray.push('```');
+    patchPartArray.push(
+      '\nDo not provide feedback yet. I will confirm once all code changes were submitted.'
+    );
 
-    var patchPart = patchPartArray.join("\n");
+    var patchPart = patchPartArray.join('\n');
     if (patchPart.length >= 15384) {
-      patchPart = patchPart.slice(0, 15384)
-      warning = 'Some parts of your patch were truncated as it was larger than 4096 tokens or 15384 characters. The review might not be as complete.'
+      patchPart = patchPart.slice(0, 15384);
+      warning =
+        'Some parts of your patch were truncated as it was larger than 4096 tokens or 15384 characters. The review might not be as complete.';
     }
     patchParts.push(patchPart);
   });
 
-  patchParts.forEach(part => {
+  patchParts.forEach((part) => {
     promptArray.push(part);
   });
 
-  promptArray.push("All code changes have been provided. Please provide me with your code review based on all the changes, context & title provided");
+  promptArray.push(
+    'All code changes have been provided. Please provide me with your code review based on all the changes, context & title provided'
+  );
 
+  console.log('callChatGPT promptArray', promptArray.join('\n'));
   // Send our prompts to ChatGPT.
   callChatGPT(
     promptArray,
     (answer) => {
-      document.getElementById('result').innerHTML = converter.makeHtml(answer + " \n\n" + warning)
+      document.getElementById('result').innerHTML = converter.makeHtml(
+        answer + ' \n\n' + warning
+      );
     },
     () => {
-      chrome.storage.session.set({ [diffPath]: document.getElementById('result').innerHTML })
-      inProgress(false)
+      chrome.storage.session.set({
+        [diffPath]: document.getElementById('result').innerHTML,
+      });
+      inProgress(false);
     }
-  )
+  );
 }
 
 async function run() {
-
   // Get current tab
   let tab = (await chrome.tabs.query({ active: true, currentWindow: true }))[0];
-  let prUrl = document.getElementById('pr-url')
-  prUrl.textContent = tab.url
+  let prUrl = document.getElementById('pr-url');
+  prUrl.textContent = tab.url;
 
-  let diffPath
-  let provider = ''
-  let error = null
-  let tokens = tab.url.split('/')
-  let context = ''
-  let title = tab.title
+  let diffPath;
+  let provider = '';
+  let error = null;
+  let tokens = tab.url.split('/');
+  let context = '';
+  let title = tab.title;
 
   // Simple verification if it would be a self-hosted GitLab instance.
-  // We verify if there is a meta tag present with the content "GitLab". 
-  let isGitLabResult = (await chrome.scripting.executeScript({
-    target:{tabId: tab.id, allFrames: true}, 
-    func: () => { return document.querySelectorAll('meta[content="GitLab"]').length }
-  }))[0];
+  // We verify if there is a meta tag present with the content "GitLab".
+  let isGitLabResult = (
+    await chrome.scripting.executeScript({
+      target: { tabId: tab.id, allFrames: true },
+      func: () => {
+        return document.querySelectorAll('meta[content="GitLab"]').length;
+      },
+    })
+  )[0];
 
   if (tokens[2] === 'github.com') {
-    provider = 'GitHub'
-  }
-  else if ("result" in isGitLabResult && isGitLabResult.result == 1) {
-    provider = 'GitLab'
+    provider = 'GitHub';
+  } else if ('result' in isGitLabResult && isGitLabResult.result == 1) {
+    provider = 'GitLab';
   }
 
   if (provider === 'GitHub' && tokens[5] === 'pull') {
@@ -238,60 +258,69 @@ async function run() {
     diffPath = `https://patch-diff.githubusercontent.com/raw/${tokens[3]}/${tokens[4]}/pull/${tokens[6]}.patch`;
     // The description of the author of the change
     // Fetch it by running a querySelector script specific to GitHub on the active tab
-    const contextExternalResult = (await chrome.scripting.executeScript({
-      target:{tabId: tab.id, allFrames: true}, 
-      func: () => { return document.querySelector('.markdown-body').textContent }
-    }))[0];
-    
-    if ("result" in contextExternalResult) {
+    const contextExternalResult = (
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id, allFrames: true },
+        func: () => {
+          return document.querySelector('.markdown-body').textContent;
+        },
+      })
+    )[0];
+
+    if ('result' in contextExternalResult) {
       context = contextExternalResult.result;
     }
-  }
-  else if (provider === 'GitLab' && tab.url.includes('/-/merge_requests/')) {
+  } else if (provider === 'GitLab' && tab.url.includes('/-/merge_requests/')) {
     // The path towards the patch file of this change
     diffPath = tab.url + '.patch';
     // The description of the author of the change
     // Fetch it by running a querySelector script specific to GitLab on the active tab
-    const contextExternalResult = (await chrome.scripting.executeScript({
-      target:{tabId: tab.id, allFrames: true}, 
-      func: () => { return document.querySelector('.description textarea').getAttribute('data-value') }
-    }))[0];
+    const contextExternalResult = (
+      await chrome.scripting.executeScript({
+        target: { tabId: tab.id, allFrames: true },
+        func: () => {
+          return document
+            .querySelector('.description textarea')
+            .getAttribute('data-value');
+        },
+      })
+    )[0];
 
-    if ("result" in contextExternalResult) {
+    if ('result' in contextExternalResult) {
       context = contextExternalResult.result;
+      console.log(context);
     }
-  }
-  else {
+  } else {
     if (provider) {
-      error = 'Please open a specific Pull Request or Merge Request on ' + provider
-    }
-    else {
-      error = 'Only GitHub or GitLab (SaaS & self-hosted) are supported.'
+      error =
+        'Please open a specific Pull Request or Merge Request on ' + provider;
+    } else {
+      error = 'Only GitHub or GitLab (SaaS & self-hosted) are supported.';
     }
   }
- 
+
   if (error != null) {
-    document.getElementById('result').innerHTML = error
+    document.getElementById('result').innerHTML = error;
     inProgress(false, true, false);
     await new Promise((r) => setTimeout(r, 4000));
     window.close();
-    return // not a pr
+    return; // not a pr
   }
 
-  inProgress(true)
+  inProgress(true);
 
-  document.getElementById("rerun-btn").onclick = () => {
-    reviewPR(diffPath, context, title)
-  }
+  document.getElementById('rerun-btn').onclick = () => {
+    reviewPR(diffPath, context, title);
+  };
 
   chrome.storage.session.get([diffPath]).then((result) => {
     if (result[diffPath]) {
-      document.getElementById('result').innerHTML = result[diffPath]
-      inProgress(false)
+      document.getElementById('result').innerHTML = result[diffPath];
+      inProgress(false);
     } else {
-      reviewPR(diffPath, context, title)
+      reviewPR(diffPath, context, title);
     }
-  })
+  });
 }
 
 run();
